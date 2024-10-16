4ead34082bf29395a08838ceb318953f88ec2d34
Akshay Mundra <akshay@gkmit.co>
2024-10-16 11:05:27 +0530

    feat: add swagger for api endpoints

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const {connectDb} = require("./db/database.js")
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger/operations.swagger.yaml'));
 
 const app = express();
 
@@ -11,6 +15,9 @@ const PORT = process.env.PORT || 3000;
 connectDb(); // connect to db
 
 app.use(express.json());
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.use('/api/operations', require('./routes/operations.route.js'))
